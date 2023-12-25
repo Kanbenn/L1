fac52311be37af4938dffe53050f16843f7c8cb5
Kanbenn <oival@ya.ru>
2023-12-25 13:16:57 +0700

    Update 02.go

--- задания/02/02.go
+++ задания/02/02.go
@@ -8,35 +8,33 @@ import (
 // Написать программу, которая конкурентно рассчитает значение квадратов чисел
 // взятых из массива (2,4,6,8,10) и выведет их квадраты в stdout.
 
-// result struct для сохранения порядка элементов исходного слайса
-type result struct {
-	i int
-	n int
+// для этого задания очень подходит Конкурентный тип вычислений,
+// который характеризуется использованием мьютексов.
+
+// result struct для обвешивания слайса мьютекс-локерами.
+type ConcurrentSlice struct {
+	sl []int
+	mu sync.RWMutex
 }
 
 func main() {
-	in := []int{2, 4, 6, 8, 10}
-
-	resultCh := make(chan result)
-	var wg sync.WaitGroup
-
-	for i := 0; i < len(in); i++ {
-		wg.Add(1)
-		go func(index, n int) {
-			defer wg.Done()
-			square := n * n
-			resultCh <- result{i: index, n: square}
-		}(i, in[i])
+	cs := ConcurrentSlice{
+		sl: []int{2, 4, 6, 8, 10},
+		mu: sync.RWMutex{},
 	}
 
-	go func() {
-		for res := range resultCh {
-			in[res.i] = res.n
-		}
-	}()
+	var wg sync.WaitGroup
+
+	for i, el := range cs.sl {
+		wg.Add(1)
+		go func(index, x int) {
+			defer wg.Done()
+			square := x * x
+			cs.sl[index] = square
+		}(i, el)
+	}
 
 	wg.Wait()
-	close(resultCh)
 
-	fmt.Println(in)
+	fmt.Println(cs.sl)
 }
