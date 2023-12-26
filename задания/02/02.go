4178cd29c41427ad3fd053ed52b79c47866166f3
Kanbenn <oival@ya.ru>
2023-12-26 19:12:17 +0700

    Update 02.go

--- задания/02/02.go
+++ задания/02/02.go
@@ -8,33 +8,62 @@ import (
 // Написать программу, которая конкурентно рассчитает значение квадратов чисел
 // взятых из массива (2,4,6,8,10) и выведет их квадраты в stdout.
 
-// для этого задания очень подходит Конкурентный тип вычислений,
-// который характеризуется использованием мьютексов.
+func main() {
 
-// result struct для обвешивания слайса мьютекс-локерами.
-type ConcurrentSlice struct {
-	sl []int
-	mu sync.RWMutex
+	slice := []int{2, 4, 6, 8, 10}
+	fmt.Println("начальный слайс: \n", slice)
+
+	fmt.Println("результат выполнения задания c помощью мьютексов: \n", SquaredSliceWithMutex(slice))
+	fmt.Println("результат выполнения задания c помощью каналов: \n", SquaredSliceWithChannels(slice))
+
 }
 
-func main() {
-	cs := ConcurrentSlice{
-		sl: []int{2, 4, 6, 8, 10},
-		mu: sync.RWMutex{},
+func SquaredSliceWithMutex(in []int) []int {
+	sliceCopy := append(in[:0:0], in...)
+
+	mu := sync.RWMutex{}
+	var wg sync.WaitGroup
+
+	for i, el := range sliceCopy {
+		wg.Add(1)
+		go func(index, n int) {
+			defer wg.Done()
+			square := n * n
+			mu.Lock()
+			sliceCopy[index] = square
+			mu.Unlock()
+		}(i, el)
 	}
+	wg.Wait()
+	return sliceCopy
+}
+
+func SquaredSliceWithChannels(in []int) []int {
+	sliceCopy := append(in[:0:0], in...)
 
 	var wg sync.WaitGroup
 
-	for i, el := range cs.sl {
+	actionCh := make(chan func())
+
+	go func() {
+		for {
+			f := <-actionCh
+			f()
+		}
+	}()
+
+	for i, el := range sliceCopy {
 		wg.Add(1)
-		go func(index, x int) {
-			defer wg.Done()
-			square := x * x
-			cs.sl[index] = square
+		go func(index, n int) {
+			f := func() {
+				square := n * n
+				sliceCopy[index] = square
+				wg.Done()
+			}
+			actionCh <- f
 		}(i, el)
 	}
-
 	wg.Wait()
 
-	fmt.Println(cs.sl)
+	return sliceCopy
 }
