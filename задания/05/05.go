3e944bc04416391f860d9c5205b0b1a81b920240
Kanbenn <oival@ya.ru>
2023-12-30 20:49:07 +0700

    upd 5 & 12

--- задания/05/05.go
+++ задания/05/05.go
@@ -1,32 +1,73 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/rs/xid"
+	"log"
+	"sync"
 	"time"
 )
 
-func main() {
-	var second time.Duration // переменная для секунд
-	fmt.Println("Add second...")
-	fmt.Scanln(&second)     // ввод
-	ch := make(chan string) // канал
-	go read(ch)             // воркер который читает
-	//запись в горутину
-	go func() {
-		for {
-			ch <- xid.New().String()
-		}
-	}()
-	//спим столько сколько указали в секундах
-	time.Sleep(second * time.Second)
-	// закрываем канал и завершаем программу
-	close(ch)
+type MyChannel struct {
+	ch       chan string
+	isClosed bool
+	once     sync.Once
 }
 
-// функция чтения из канала и вывод значений в консоль
-func read(ch chan string) {
-	for i := range ch {
-		fmt.Println(i)
+func (mc *MyChannel) SafeClose() {
+	log.Println("SafeClose: сэйфово закрываю канал")
+	mc.once.Do(func() {
+		close(mc.ch)
+		mc.isClosed = true
+	})
+}
+
+func main() {
+	var seconds int
+	flag.IntVar(&seconds, "s", 10, "количество секунд работы программы. дефолт: 10")
+	flag.Parse()
+	fmt.Println("количество секунд работы программы:", seconds)
+
+	mc := &MyChannel{ch: make(chan string)}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(4)
+
+	go writer(mc, wg) // запуск пишущей горутины
+	go writer(mc, wg) // запуск пишущей горутины
+
+	go reader(mc, wg) // запуск читающей горутины
+	go reader(mc, wg) // запуск читающей горутины
+
+	// даём программе отработать указанное количество секунд
+	time.Sleep(time.Second * time.Duration(seconds))
+	log.Println("main: время вышло")
+
+	mc.SafeClose() // закрываем канал
+
+	// ждём когда writer закроет канал и завершится,
+	// после чего range в reader'e среагирует и прекратит чтение.
+	wg.Wait()
+
+}
+
+func writer(mc *MyChannel, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		if mc.isClosed {
+			log.Println("writer: канал закрыт, выходим")
+			return
+		}
+		mc.ch <- "."
+		time.Sleep(100 * time.Millisecond) // пауза 100 мс чтобы не спамить в консоль.
 	}
+}
+
+func reader(mc *MyChannel, wg *sync.WaitGroup) {
+	defer wg.Done()
+	log.Println("reader: начинаю отправку данных в канал")
+	for data := range mc.ch { // range корректно среагирует на раннее закрытие канала
+		fmt.Print(data)
+	}
+	log.Println("reader: читать больше нечего, выходим")
 }
--- задания/12/12.go
+++ задания/12/12.go
@@ -1,18 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
-// в го нет метода реализации множеств, но можем использовать тим мап
+// в голанге нет типа "множество" как set в питоне. можно сымитировать через тип Map.
+// но конкретно для этой задачи более эффективный алгоритм - сортировка слайса.
+
 func main() {
-	data := []string{"cat", "cat", "dog", "cat", "tree"}
+	input := []string{"cat", "cat", "dog", "cat", "tree"}
 
-	fmt.Println(set(data))
+	fmt.Println("начальный слайс:", input)
+
+	fmt.Println("setViaMap:", setViaMap(input))
+
+	fmt.Println("setViaSortedSlice:", setViaSortedSlice(input))
 }
 
-func set(data []string) map[string]bool {
-	mp := make(map[string]bool)
-	for _, i := range data {
-		mp[i] = true
+func setViaMap(in []string) map[string]struct{} {
+	// пустой struct занимает 0 байт, в отличии от bool который 1 байт.
+	// в голанге это юзают как лайфхак для заглушки в Мапах или
+	// пустого синала в Каналах. Смотри также: Устный Вопрос №4.
+	m := make(map[string]struct{}, len(in))
+	for _, i := range in {
+		m[i] = struct{}{}
 	}
-	return mp
+	return m
+}
+
+func setViaSortedSlice(in []string) []string {
+	slices.Sort(in)
+
+	out := make([]string, 0, len(in))
+	var prev string
+	for _, cur := range in {
+		if cur != prev {
+			out = append(out, cur)
+			prev = cur
+		}
+	}
+	// обрезаю capacity до получившегося количества элементов.
+	return out[:len(out):len(out)]
 }
