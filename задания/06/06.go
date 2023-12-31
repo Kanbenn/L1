f36413461a6172e4186e3a613ea9cc8954b7cd14
Kanbenn <oival@ya.ru>
2023-12-31 23:12:58 +0700

    Update 06.go

--- задания/06/06.go
+++ задания/06/06.go
@@ -7,12 +7,29 @@ import (
 	"time"
 )
 
-//В Go есть несколько способов остановить выполнение горутины:
-//1. Возврат из функции: Возвращение из функции, вызывающей горутину, автоматически останавливает выполнение горутины.
-//2. Использование канала: Вы можете использовать канал для уведомления горутины о необходимости остановки. Горутина может регулярно проверять значение в канале и завершить выполнение, если получит указанный сигнал.
-//3. Использование флага: Вы можете использовать булеву переменную в качестве флага, которая будет указывать, когда горутина должна быть остановлена. Горутина может регулярно проверять значение этого флага и завершить выполнение, если значение true.
-//4. Использование функции context: Пакет context в Go предоставляет возможность передавать сигналы о прекращении выполнения через контекст. Горутина может проверять контекст и завершить выполнение, если получит сигнал отмены.
-//5. Использование таймера: Горутина может останавливаться после определенного времени с помощью использования таймера. Вы можете использовать пакет time для этого.
+// Задание:
+// Реализовать все возможные способы остановки выполнения горутины.
+
+// Ответ:
+// Зная коварность менторов WB, нужно уточнить вопрос: как Завершить горутину либо
+// временно Приостановить её выполнение и вернуть управление рантайму?
+
+// Завершить горутину извне в Go возможности нет (на момент версии го 1.22),
+// потому и возникают ошибки deadlock или all goroutines are asleep.
+// В этом большое отличие легковесных горутин рантайма Go от полновесных процессов ОС,
+// которые могут реагировать на SIG INTERRUPT, SIG TERMINATE и тд, а в крайнем
+// случае SIGKILL грубо вырубает процесс, не дожидаясь от него ответа.
+
+// Горутина приостанавливается и передаёт управление рантайму (sysmon) при:
+// вызове других горутин/функций, ожидание канала, мютекса, таймера, контекста и др.
+// Ещё один способ приостановить горутину - запустить сборщик мусора вызовом runtime.gogc()
+// (применяется в редких случаях, для тестов)
+
+// Завершиться горутина должна сама инструкцией return.
+
+// Для само-регуляции приостанавливающихся горутин в голанге юзают
+// бесконечный цикл с select-switch'ем внутри, ожидающем сигнала
+// дальнейших действий из одного или нескольких внешних источников.
 
 // использование таймера
 func timerGoroutine() {
@@ -21,10 +38,10 @@ func timerGoroutine() {
 	for {
 		select {
 		case <-timer.C:
-			fmt.Println("Stopping goroutine...")
+			fmt.Println("Stopping timerGoroutine...")
 			return
 		default:
-			fmt.Println("Working...")
+			fmt.Println("timerGoroutine working...")
 			time.Sleep(time.Second)
 		}
 	}
@@ -32,23 +49,23 @@ func timerGoroutine() {
 
 // с использованием time
 func timeGoroutine() {
+	fmt.Println("timeGoroutine sleeping...")
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
 		time.Sleep(time.Second)
 	}
+	fmt.Println("timeGoroutine woke up.")
 }
 
 // с использованием bool переменных
-func boolGoroutine(ch chan bool) {
+func boolGoroutine(ch chan struct{}) {
 	for {
 		select {
-		case msg := <-ch:
-			if msg == true {
-				fmt.Println("Stopping goroutine...")
-				return
-			}
+		case <-ch:
+			fmt.Println("Stopping boolGoroutine...")
+			return
 		default:
-			fmt.Println("Working...")
+			fmt.Println("boolGoroutine working...")
 			time.Sleep(time.Second)
 		}
 	}
@@ -60,10 +77,10 @@ func flagGoroutine() {
 	defer wg.Done()
 	for {
 		if stopFlag {
-			fmt.Println("Stopping goroutine...")
+			fmt.Println("Stopping flagGoroutine...")
 			return
 		}
-		fmt.Println("Working...")
+		fmt.Println("flagGoroutine working...")
 		time.Sleep(time.Second)
 	}
 }
@@ -73,10 +90,10 @@ func ctxGoroutine(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
-			fmt.Println("Stopping goroutine...")
+			fmt.Println("Stopping ctxGoroutine...")
 			return
 		default:
-			fmt.Println("Working...")
+			fmt.Println("ctxGoroutine working...")
 			time.Sleep(1 * time.Second)
 		}
 	}
@@ -88,29 +105,24 @@ var wg sync.WaitGroup
 func main() {
 	go timerGoroutine()
 	time.Sleep(6 * time.Second)
-	fmt.Println("Exiting... timerGoroutine")
 
-	ch := make(chan bool)
+	ch := make(chan struct{})
 	go boolGoroutine(ch)
 	time.Sleep(5 * time.Second)
-	ch <- true
-	fmt.Println("Exiting... boolGoroutine")
+	ch <- struct{}{}
 
 	wg.Add(1)
 	go flagGoroutine()
 	time.Sleep(5 * time.Second)
-	fmt.Println("Exiting... flagGoroutine")
 	stopFlag = true
 	wg.Wait()
 
 	ctx, cancel := context.WithCancel(context.Background())
 	go ctxGoroutine(ctx)
 	time.Sleep(5 * time.Second)
-	fmt.Println("Exiting... ctxGoroutine")
 	cancel()
 	time.Sleep(1 * time.Second)
 
 	go timeGoroutine()
 	time.Sleep(5 * time.Second)
-	fmt.Println("Exiting... timeGoroutine")
 }
