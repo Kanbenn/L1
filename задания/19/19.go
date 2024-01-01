8c96d84b37d1d01ab0d936c6ee049767ca46800c
Kanbenn <oival@ya.ru>
2024-01-01 10:33:27 +0700

    Update 19.go

--- задания/19/19.go
+++ задания/19/19.go
@@ -1,22 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strings"
+)
 
 func main() {
-	// переменная для строки
-	var str string
-	fmt.Println("Add string")
-	fmt.Scanln(&str)      //ввод
-	newstr := []rune(str) // преобразуем строку в руну для работы с unicode
-	i := 0                // определяем начальную точку массива рун
-	j := len(newstr) - 1  // определяем конечную точку массива рун
 
-	// простой цикл в котором меняем местами значений массива рун по индексам i,j
-	for i < j {
-		newstr[i], newstr[j] = newstr[j], newstr[i]
-		i++
-		j--
-	}
-	// преобразуем руны в строку и выводим результат
-	fmt.Println(string(newstr))
+	fmt.Println("Введите строку для разворота:")
+
+	// fmt.Scanln почему-то читает не всю строку, а лишь одно слово
+	// до пробела, поэтому сделал через bufio.
+	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	// убираю ненужные символы перевода каретки
+	str := strings.ReplaceAll(input, "\r\n", "")
+	fmt.Printf("%q \n", str)
+
+	runes := []rune(str)
+	fmt.Printf("%q \n", runes)
+	slices.Reverse(runes)
+
+	fmt.Println(string(runes))
 }
