05574f3e6a6856476925056f7640edc6a8107849
Kanbenn <oival@ya.ru>
2024-01-01 10:38:33 +0700

    Update 20.go

--- задания/20/20.go
+++ задания/20/20.go
@@ -1,36 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"slices"
 	"strings"
 )
 
 func main() {
-	str := "snow dog sun"             // строка
-	reversed := reverse(str)          //переворачиваем слова в строке
-	words := strings.Fields(reversed) // переворачиваем строку
-	for i := 0; i < len(words)/2; i++ {
-		words[i], words[len(words)-1-i] = words[len(words)-1-i], words[i]
-	}
-	fmt.Println(strings.Join(words, " "))
-}
+	fmt.Println("Введите несколько слов для их разворота:")
 
-func reverse(str string) string {
-	words := strings.Split(str, " ")       //  убираем пробелы получаем массив
-	reversed := make([]string, len(words)) // массив перевернутых
-	for i := 0; i < len(words); i++ {
-		reversed[i] = reverseWord([]rune(words[i])) // переворачиваем каждое слово в цикле
-	}
-	return strings.Join(reversed, " ") // возвращаем результат в строке объединяя пробелом
-}
+	// fmt.Scanln почему-то читает не всю строку, а лишь одно слово
+	// до пробела, поэтому сделал через bufio.
+	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 
-func reverseWord(str []rune) string {
-	i := 0
-	j := len(str) - 1
-	for i < j {
-		str[i], str[j] = str[j], str[i]
-		i++
-		j--
-	}
-	return string(str)
+	// убираю ненужные символы перевода каретки
+	str := strings.ReplaceAll(input, "\r\n", "")
+	fmt.Printf("%q \n", str)
+
+	sliceOfStrings := strings.Split(str, " ")
+
+	slices.Reverse(sliceOfStrings)
+
+	fmt.Println(strings.Join(sliceOfStrings, " "))
 }
